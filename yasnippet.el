dab4fea57ffeef6b6a79b4445816075b89b069a4
João Távora <joaotavora@gmail.com>
2012-05-29 21:38:25 +0100

    enhacement: simplify doc in yasnippet.el header

--- yasnippet.el
+++ yasnippet.el
@@ -35,7 +35,6 @@
 ;;    (require 'yasnippet)
 ;;    (yas/global-mode 1)
 ;;
-;;
 ;;   Interesting variables are:
 ;;
 ;;       `yas/snippet-dirs'
@@ -75,46 +74,34 @@
 ;;
 ;;       M-x yas/insert-snippet
 ;;
-;;           Prompts you for possible snippet expansion if that is
-;;           possible according to buffer-local and snippet-local
-;;           expansion conditions.  With prefix argument, ignore these
-;;           conditions.
+;;           Prompts you for a snippet to expand. With prefix arg,
+;;           ignore buffer-local and snippet-specific conditions.
 ;;
 ;;       M-x yas/visit-snippet-file
 ;;
-;;           Prompts you for possible snippet expansions like
-;;           `yas/insert-snippet', but instead of expanding it, takes
-;;           you directly to the snippet definition's file, if it
-;;           exists.
+;;           Visit an existing, active snippet in a `snippet-mode' buffer.
 ;;
 ;;       M-x yas/new-snippet
 ;;
-;;           Lets you create a new snippet file in the correct
-;;           subdirectory of `yas/snippet-dirs', according to the
-;;           active major mode.
+;;           Pop a new buffer in `snippet-mode' so you can create a
+;;           new snippet.
 ;;
 ;;       M-x yas/load-snippet-buffer
 ;;
-;;           When editing a snippet, this loads the snippet.  This is
-;;           bound to "C-c C-c" while in the `snippet-mode' editing
-;;           mode.
+;;           In `snippet-mode', load a snippet into the active
+;;           tables. Bound to "C-c C-c".
 ;;
 ;;       M-x yas/tryout-snippet
 ;;
-;;           When editing a snippet, this opens a new empty buffer,
-;;           sets it to the appropriate major mode and inserts the
-;;           snippet there, so you can see what it looks like.  This is
-;;           bound to "C-c C-t" while in `snippet-mode'.
+;;           In `snippet-mode', test a snippet so you can see what it
+;;           looks like. Bound to "C-c C-t".
 ;;
 ;;       M-x yas/describe-tables
 ;;
-;;           Lists known snippets in a separate buffer. User is
-;;           prompted as to whether only the currently active tables
-;;           are to be displayed, or all the tables for all major
-;;           modes.
+;;           Lists known snippets in a separate buffer.
 ;;
 ;;   The `dropdown-list.el' extension is bundled with YASnippet, you
-;;   can optionally use it the preferred "prompting method", puting in
+;;   can optionally use it as preferred "prompting method", puting in
 ;;   your .emacs file, for example:
 ;;
 ;;       (require 'dropdown-list)
@@ -125,11 +112,6 @@
 ;;   Also check out the customization group
 ;;
 ;;        M-x customize-group RET yasnippet RET
-;;
-;;   If you use the customization group to set variables
-;;   `yas/snippet-dirs' or `yas/global-mode', make sure the path to
-;;   "yasnippet.el" is present in the `load-path' *before* the
-;;   `custom-set-variables' is executed in your .emacs file.
 ;;
 ;;   For more information and detailed usage, refer to the project page:
 ;;      http://github.com/capitaomorte/yasnippet
